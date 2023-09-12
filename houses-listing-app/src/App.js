cab0faaafe37b90a8ded84e30ca0842404a130be
Farida NDiaye <faridandiaye60@gmail.com>
2023-09-12 01:48:14 -0400

    APP.JS FILE UPDATED

--- houses-listing-app/src/App.js
+++ houses-listing-app/src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+// import React from 'react';
+// import './App.css';
+// import HouseList from './HouseList';
+// import AddListing from './AddListing';
+// import UserAddedHouses from './UserAddedHouses';
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//       <header className="App-header">
+//         <h1>Houses Listing App</h1>
+//       </header>
+//       {/* <AddListing /> */}
+//       <UserAddedHouses />
+//       {/* <HouseList /> */}
+//     </div>
+//     </Router>
+    
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
 import './App.css';
+import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; // Import Router and related components
+import HouseList from './HouseList';
+import Home from './Home';
+import AddListing from './AddListing';
+import UserAddedHouses from './UserAddedHouses';
+import House from './House';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Houses Listing App</h1>
+
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            <li className='navbar'><Link to="/">Home</Link></li>
+            <li className='navbar'><Link to="/user-added-houses">Add House</Link></li>
+            <li className='navbar'>
+              <a
+                className="App-link"
+                href="https://kololi.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                View Houses
+              </a>
+        </li>
+            
+          </ul>
+          
+        </header>
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user-added-houses" element={<UserAddedHouses />} />
+          <Route path="/house-list" element={<HouseList />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
