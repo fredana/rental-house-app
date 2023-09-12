1f8210d0fe0b3d9e54d070e83dcceedd383a2b58
Farida NDiaye <faridandiaye60@gmail.com>
2023-09-12 01:44:20 -0400

    HOUSELIST.JS FILE UPDATED

---
+++ houses-listing-app/src/HouseList.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import House from './House';
+
+const HouseList = () => {
+  const [houses, setHouses] = useState([]);
+  const [showList, setShowList] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/houses')
+      .then((response) => response.json())
+      .then((data) => setHouses(data));
+  }, []);
+
+  const toggleList = () => {
+    setShowList(!showList);
+  };
+
+  return (
+    <div className="house-list">
+      <button onClick={toggleList}>
+        {showList ? 'Hide House List' : 'Show House List'}
+      </button>
+      {showList && (
+        <div>
+          <h2>Houses List</h2>
+          {houses.map((house) => (
+            <House key={house.id} house={house} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HouseList;
